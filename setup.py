1964ac7a21e2e8e3e6e84ec7eb0577f28f3395e6
enes <3nesslbr@gmail.com>
2025-12-13 22:57:55 +0300

    Update package name in setup.py
    Changed the package name from 'transformer-model-suite' to 'model'.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
-    name="transformer-model-suite",
+    name="model" #will change when its published,
     version="0.1.0",
     author="San1ura",
     author_email="",
